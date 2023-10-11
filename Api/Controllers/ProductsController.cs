4df4cf7beaf800676971ae8ddf7e1b553a94e9fc
mohamed-shokrani <mohamedshokrani55@gmail.com>
2023-10-12 00:05:56 +0300

    add authorization

--- Api/Controllers/ProductsController.cs
+++ Api/Controllers/ProductsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Api.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-//[Authorize]
+[Authorize]
 public class ProductsController : ControllerBase
 {
     private readonly IProductRepository _productRepository;
